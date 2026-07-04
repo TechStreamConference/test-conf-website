3140c004c9ccee6b80e102f463003cbea195af5d
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-05 00:43:23 +0200

    feat: generate frontend types

---
+++ frontend/scripts/gen-types.ts
@@ -0,0 +1,30 @@
+import { parseArgs } from 'node:util';
+import { createClient } from '@hey-api/openapi-ts';
+
+const {
+    values: { i: input, o: output }
+} = parseArgs({
+    options: {
+        i: {
+            type: 'string',
+            short: 'i'
+        },
+        o: {
+            type: 'string',
+            short: 'o'
+        }
+    }
+});
+
+if (!input || !output) {
+    console.error('Usage: generate -i <input> -o <output>');
+    process.exit(1);
+}
+
+await createClient({
+    input: input,
+    output: {
+        clean: false,
+        path: output,
+    }
+});
