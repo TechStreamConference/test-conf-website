aba194f7887347bed698199366642b56262ad758
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-05 16:15:12 +0200

    feat: exclute generated file from linting and formatting

--- frontend/scripts/gen-types.ts
+++ frontend/scripts/gen-types.ts
@@ -2,29 +2,29 @@ import { parseArgs } from 'node:util';
 import { createClient } from '@hey-api/openapi-ts';
 
 const {
-    values: { i: input, o: output }
+	values: { i: input, o: output }
 } = parseArgs({
-    options: {
-        i: {
-            type: 'string',
-            short: 'i'
-        },
-        o: {
-            type: 'string',
-            short: 'o'
-        }
-    }
+	options: {
+		i: {
+			type: 'string',
+			short: 'i'
+		},
+		o: {
+			type: 'string',
+			short: 'o'
+		}
+	}
 });
 
 if (!input || !output) {
-    console.error('Usage: generate -i <input> -o <output>');
-    process.exit(1);
+	console.error('Usage: generate -i <input> -o <output>');
+	process.exit(1);
 }
 
 await createClient({
-    input: input,
-    output: {
-        clean: false,
-        path: output,
-    }
+	input: input,
+	output: {
+		clean: false,
+		path: output
+	}
 });
