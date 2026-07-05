d98b385efbd58e250f1eb64f1aa6d50060e10a99
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-05 16:14:37 +0200

    fix: formatter

--- backend/src/backend/models/responses.py
+++ backend/src/backend/models/responses.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 # class name should include the api version since the typescript generator uses the same name.
 # This could lead to confusion within the frontend once a second api version gets introduced.
 
+
 @final
 class GlobalsResponseV1(BaseModel):
     footer_text: str
