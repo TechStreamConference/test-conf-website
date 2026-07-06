f257cd3767e4f8d0dfb512257f18e6abf5fbdfac
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-06 22:31:22 +0200

    fix: handle undefined data while fatching

---
+++ frontend/src/bff/errors.ts
@@ -0,0 +1,6 @@
+export class UndefinedDataError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "UndefinedDataError"
+    }
+}
--- frontend/src/bff/v1/globals.server.ts
+++ frontend/src/bff/v1/globals.server.ts
@@ -2,12 +2,17 @@ import '$bff/client';
 
 import { getGlobalsV1 } from '$gen/sdk.gen';
 import type { GlobalsResponseV1 } from '$gen/types.gen';
+import { UndefinedDataError } from '$bff/errors';
 
 export async function loadGlobals(): Promise<GlobalsResponseV1> {
 	const { data, error } = await getGlobalsV1();
 
-	if (error || data == undefined) {
+	if (error) {
 		throw error;
+	}
+
+	if (data == undefined) {
+		throw new UndefinedDataError("loadGlobals")
 	}
 
 	return data;
