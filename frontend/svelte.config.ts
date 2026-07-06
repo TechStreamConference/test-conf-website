3251295bb9669575cf5322c21004a24f5c299562
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-06 21:39:41 +0200

    fix: remove comment

--- frontend/svelte.config.ts
+++ frontend/svelte.config.ts
@@ -6,7 +6,6 @@ const config: Config = {
 	preprocess: vitePreprocess(),
 
 	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 
