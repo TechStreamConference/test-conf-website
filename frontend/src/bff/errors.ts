9db9431343c9b21edd045ce10640e64fb8505db6
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-10 21:56:54 +0200

    fix: use generic error type

--- frontend/src/bff/errors.ts
+++ frontend/src/bff/errors.ts
@@ -1,3 +1,12 @@
+export class GenericBackendError<T = unknown> extends Error {
+	error: T;
+	constructor(message: string, error: T) {
+		super(message);
+		this.name = 'GenericBackendError';
+		this.error = error;
+	}
+}
+
 export class UndefinedDataError extends Error {
 	constructor(message: string) {
 		super(message);
--- frontend/src/bff/v1/globals.server.ts
+++ frontend/src/bff/v1/globals.server.ts
@@ -3,12 +3,13 @@ import '$bff/client';
 import { getGlobalsV1 } from '$gen/sdk.gen';
 import type { GlobalsResponseV1 } from '$gen/types.gen';
 import { UndefinedDataError } from '$bff/errors';
+import { GenericBackendError } from '$bff/errors';
 
 export async function loadGlobals(): Promise<GlobalsResponseV1> {
 	const { data, error } = await getGlobalsV1();
 
 	if (error) {
-		throw error;
+		throw new GenericBackendError('loadGlibals', error);
 	}
 
 	if (data == undefined) {
