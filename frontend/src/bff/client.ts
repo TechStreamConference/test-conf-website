2d263b5dd6221078ca6985e631fe5c109cd39072
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-15 21:54:38 +0200

    fix: env ci

--- frontend/src/bff/client.ts
+++ frontend/src/bff/client.ts
@@ -1,7 +1,11 @@
 import { client } from '$gen/client.gen';
 import { env } from '$env/dynamic/private';
 
-const BACKEND_URL: string = env.FRONTEND_API_BASE_URL;
+const BACKEND_URL: string | undefined = env['FRONTEND_API_BASE_URL'];
+
+if (!BACKEND_URL) {
+	throw new Error('FRONTEND_API_BASE_URL is not configured');
+}
 
 client.setConfig({
 	baseUrl: BACKEND_URL
