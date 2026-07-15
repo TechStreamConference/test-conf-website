8b953d60651b2adebb6f2577c454f0ffd5cd1dd5
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-15 21:27:55 +0200

    fix: use .env variable

--- frontend/src/bff/client.ts
+++ frontend/src/bff/client.ts
@@ -1,6 +1,7 @@
 import { client } from '$gen/client.gen';
+import { env } from '$env/dynamic/private';
 
-const BACKEND_URL: string = 'http://localhost/api'; // replace with docker variable
+const BACKEND_URL: string = env.FRONTEND_API_BASE_URL;
 
 client.setConfig({
 	baseUrl: BACKEND_URL
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,50 @@
+import path from 'node:path';
+
 import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**']
-				}
-			},
+export default defineConfig(({ mode }) => {
+	// Load ../.env into process.env
+	Object.assign(process.env, loadEnv(mode, path.resolve(__dirname, '..'), ''));
 
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+	return {
+		plugins: [sveltekit()],
+
+		test: {
+			expect: { requireAssertions: true },
+
+			projects: [
+				{
+					extends: './vite.config.ts',
+
+					test: {
+						name: 'client',
+
+						browser: {
+							enabled: true,
+							provider: playwright(),
+							instances: [{ browser: 'chromium', headless: true }]
+						},
+
+						include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+						exclude: ['src/lib/server/**']
+					}
+				},
+
+				{
+					extends: './vite.config.ts',
+
+					test: {
+						name: 'server',
+						environment: 'node',
+
+						include: ['src/**/*.{test,spec}.{js,ts}'],
+						exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					}
 				}
-			}
-		]
-	}
+			]
+		}
+	};
 });
