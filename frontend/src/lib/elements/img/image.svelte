f378efd4fc8ae1422089f8a4f49d0087bd57ab0d
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-07-24 20:36:29 +0200

    feat: image component

---
+++ frontend/src/lib/elements/img/image.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	interface Props {
+		src: string;
+		alt: string;
+		height?: string;
+		width?: string;
+		classes?: string;
+	}
+
+	const { src, alt, height, width, classes }: Props = $props();
+</script>
+
+<img {src} {alt} class={classes} style:width style:height />
+
+<style>
+	img {
+		display: block;
+		height: auto;
+		width: auto;
+	}
+</style>
