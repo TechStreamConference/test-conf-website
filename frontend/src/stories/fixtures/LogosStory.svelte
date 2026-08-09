ce349ef024ea2471c66edb22b64baa51ae927c1e
Michael Gerhold <michael.gerhold@web.de>
2026-08-09 12:33:57 +0200

    docs: add stories for pending UI components

---
+++ frontend/src/stories/fixtures/LogosStory.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import LogoBig from '$lib/elements/img/logo_big.svelte';
+	import LogoSmall from '$lib/elements/img/logo_small.svelte';
+
+	interface Props {
+		variant: 'big' | 'small';
+		width: string;
+	}
+
+	let { variant, width }: Props = $props();
+</script>
+
+{#if variant === 'big'}
+	<LogoBig {width} />
+{:else}
+	<LogoSmall {width} />
+{/if}
