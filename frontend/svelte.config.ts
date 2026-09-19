3c87c78f683cfc32edddc611f35cc6411ee7fac6
PurpurTentakel97 <martin.schreurs@netcologne.de>
2026-09-19 13:47:02 +0200

    fix: config

--- frontend/svelte.config.ts
+++ frontend/svelte.config.ts
@@ -3,22 +3,23 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import type { Config } from '@sveltejs/kit';
 
 const config: Config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
+    compilerOptions: {
+        runes: ({ filename }) =>
+            filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+    },
 
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$gen: 'src/generated',
-			$bff: 'src/bff',
-			$lib: 'src/lib',
-			$logging: 'src/logging',
-			$src: 'src'
-		}
-	}
+    kit: {
+        adapter: adapter(),
+        alias: {
+            $gen: 'src/generated',
+            $bff: 'src/bff',
+            $lib: 'src/lib',
+            $logging: 'src/logging',
+            $src: 'src'
+        }
+    }
 };
 
 export default config;
